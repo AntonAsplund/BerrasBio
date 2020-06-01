2ad6a1a6e615c192b25a64b9ee870b536f4f6ce3
Daniel Andersson <danielandersson_83@hotmail.com>
2020-06-01 17:24:42 +0200

    Uppdaterade databasen med fler properties i Movies, en relation till User i Order och ändrade namn på User modelen. Fixade också lite buggar som dök upp.

--- BerrasBio/Models/User.cs
+++ BerrasBio/Models/User.cs
@@ -5,9 +5,9 @@ using System.Threading.Tasks;
 
 namespace BerrasBio.Models
 {
-    public class UserCredentials
+    public class User
     {
-        public int Id { get; set; }
+        public int UserId { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
         public bool IsAdmin { get; set; }
