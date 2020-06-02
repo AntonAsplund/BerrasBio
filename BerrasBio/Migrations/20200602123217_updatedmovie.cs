4d32ac113d5f1434f7effa1b53c1e7bd5e361a1c
Jonathan Krantz <jonathan.krantz@edu.varberg.se>
2020-06-02 14:33:25 +0200

    migration

---
+++ BerrasBio/Migrations/20200602123217_updatedmovie.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BerrasBio.Migrations
+{
+    public partial class updatedmovie : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Movies");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Movies",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
